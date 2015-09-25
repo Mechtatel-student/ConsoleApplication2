5c6f036ddec02a62d42a37c6590d2e48b8a2b5de
Mechtatel-student <Mechtatel._T@mail.ru>
2015-09-25 16:38:04 +0300

    UnitTests

---
+++ ClassLibrary1/TestMainProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ConsoleApplication1;
+
+namespace ClassLibrary1
+{
+    [TestFixture, Description("Testing main static methods")]
+    class TestMainProgram
+    {
+        [Test]
+        public void TestSum()
+        {
+            Assert.AreEqual(15, Program.Sum(10, 5));
+            Assert.AreEqual(15, Program.Sum(5,10));
+        }
+
+        [TestCase]
+        [TestCase(15,5,10)]
+        [TestCase(34,45,-11)]
+        public void TestCaseSum(int result,int op1, int op2)
+        {
+            Assert.DoesNotThrow();
+            Assert.AreEqual(result, Program.Sum(op1,op2));
+            
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static int Sum(int a, int b)
+        {
+            //int c=0;
+            //try
+            //{
+            //    c=a+b;
+            //    return c;
+            //}
+            //catch
+            //{
+            //    tr
+            //}
+            return a + b;
+            
+           
+        }
+    }
+    }
